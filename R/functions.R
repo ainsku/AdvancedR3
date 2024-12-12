7ba3aa503f01ae752c40510453bfade244020bac
ainsku <ainsku12@gmail.com>
2024-12-12 09:20:55 +0100

    created multiple steps of the workflow

--- R/functions.R
+++ R/functions.R
@@ -4,19 +4,19 @@
 #'
 #' @return A data.frame/tibble.
 descriptive_stats <- function(data) {
-    data |>
-        dplyr::group_by(metabolite) |>
-        dplyr::summarise(dplyr::across(
-            value,
-            list(
-                mean = mean,
-                sd = sd
-            )
-        )) |>
-        dplyr::mutate(dplyr::across(
-            tidyselect::where(is.numeric),
-            ~ round(.x, digits = 1)
-        ))
+  data |>
+    dplyr::group_by(metabolite) |>
+    dplyr::summarise(dplyr::across(
+      value,
+      list(
+        mean = mean,
+        sd = sd
+      )
+    )) |>
+    dplyr::mutate(dplyr::across(
+      tidyselect::where(is.numeric),
+      ~ round(.x, digits = 1)
+    ))
 }
 
 #' A histogram of metabolite distributions
@@ -25,7 +25,74 @@ descriptive_stats <- function(data) {
 #'
 #' @return A plot object
 plot_distributions <- function(data) {
-    ggplot2::ggplot(data, ggplot2::aes(x = value)) +
-        ggplot2::geom_histogram() +
-        ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+  ggplot2::ggplot(data, ggplot2::aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(ggplot2::vars(metabolite), scales = "free")
+}
+
+#' Convert a column's character values to snake case format
+#'
+#' @param data the lipidomics dataset
+#' @param columns the column you want to convert to snake case
+#'
+#' @return A data frame
+column_values_to_snake_case <- function(data, columns) {
+  data |> dplyr::mutate(dplyr::across({{ columns }}, snakecase::to_snake_case))
+}
+
+#' Convert the metabolite long format into a wider one.
+#'
+#' @param data The lipidomics dataset.
+#'
+#' @return A wide data frame.
+metabolites_to_wider <- function(data) {
+  data |>
+    tidyr::pivot_wider(
+      names_from = metabolite,
+      values_from = value,
+      values_fn = mean,
+      names_prefix = "metabolite_"
+    )
+}
+
+#' A transformation recipe to pre-process the data.
+#'
+#' @param data the lipidomics dataset
+#' @param metabolite_variable the column of the metabolite varaible
+#'
+#' @return a recipe object
+create_recipe_spec <- function(data, metabolite_variable) {
+  recipes::recipe(data) |>
+    recipes::update_role({{ metabolite_variable }},
+      age,
+      gender,
+      new_role = "predictor"
+    ) |>
+    recipes::update_role(class,
+      new_role = "outcome"
+    ) |>
+    recipes::step_normalize(tidyselect::starts_with("metabolite_"))
+}
+
+#' Create a workflow object of the model and transformations.
+#'
+#' @param model_specs The model specs
+#' @param recipe_specs The recipe specs
+#'
+#' @return A workflow object
+create_model_workflow <- function(model_specs, recipe_specs) {
+  workflows::workflow() |>
+    workflows::add_model(model_specs) |>
+    workflows::add_recipe(recipe_specs)
+}
+
+#' Create a tidy output of the model results.
+#'
+#' @param workflow_fitted_model The model workflow object that has been fitted.
+#'
+#' @return A data frame
+tidy_model_output <- function(workflow_fitted_model) {
+  workflow_fitted_model |>
+    workflows::extract_fit_parsnip() |>
+    broom::tidy(exponentiate = TRUE)
 }
